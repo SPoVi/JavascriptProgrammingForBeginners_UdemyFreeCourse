8c905afdce47264f6cc4e59eb45f19bebfa2f67a
S.Pons <snop.vlc@gmail.com>
2022-04-12 15:09:37 +0200

    Update - S1 basic.js - Var and const

--- S1 - The Basics of Javascript/basic.js
+++ S1 - The Basics of Javascript/basic.js
@@ -1,2 +1,17 @@
-console.log("Hello World");
+//console.log("Hello World");
 // This is a comment: the above line prints something to the console
+
+// Variables and Constants
+// Keywords -> let and const
+
+let name = "Spovi";
+console.log(name);
+
+name = "SPV";
+console.log(name);
+
+const age = 18;
+console.log(age);
+
+// The next line produces an error, because is trying to change a const var
+//age = 20;
