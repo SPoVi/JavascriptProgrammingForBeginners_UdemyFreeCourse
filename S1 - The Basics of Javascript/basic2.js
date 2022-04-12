4ed17cb5714d1992e8c978b4a8022f545c6a70bf
S.Pons <snop.vlc@gmail.com>
2022-04-12 15:14:42 +0200

    Add - S1 - Data types basic2.js index2.html

---
+++ S1 - The Basics of Javascript/basic2.js
@@ -0,0 +1,16 @@
+// Data Types -> It means the type of data we can store in a variable or a constant
+// Data Types in Javascript are :-
+// String, Numbre, Boolean, null, undefined
+
+const myName = "Spovi";
+const myInteger = 89;
+const myDecimal = 90.23;
+
+const myBool = false;
+const myNull = null;
+
+const myUndefinedConst = undefined;
+let x;
+
+console.log(typeof myName);
+console.log(typeof myInteger);
