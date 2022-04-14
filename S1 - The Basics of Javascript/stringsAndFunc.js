4dc1ab7d103524400e1a2e4f72e9394266bce1ce
S.Pons <snop.vlc@gmail.com>
2022-04-14 18:32:09 +0200

    Add - S1 - String and Func

---
+++ S1 - The Basics of Javascript/stringsAndFunc.js
@@ -0,0 +1,18 @@
+var firstName = "Nombre";
+var lastName = "Apellido1";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var fullName2 = `${firstName} ${lastName} is my full name`;
+console.log(fullName2);
+
+// To upper Case func (method)
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
+
+// Lenght method
+console.log(fullName.length)
+
+// Special char \
+var message = 'It\'s ok';
+console.log(message)
